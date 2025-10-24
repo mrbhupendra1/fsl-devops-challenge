cff9099b23815630fa9425e41aeb073c67cc912d
Bhupendra Kumar <mrbhupendra333@gmail.com>
2025-10-24 10:37:18 +0000

    Initial commit - app + docker + k8s + ci

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  const secret = process.env.MY_SECRET || 'NO_SECRET';
+  res.send(`<h2>FSL DevOps Challenge</h2><p>MY_SECRET: ${secret}</p>`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
